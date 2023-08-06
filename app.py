489d977479eead4be1308a4210af3d088bfb8d3f
Alexandre <=>
2023-08-06 15:28:40 -0300

    fixign cache to work and inserting formatted logging

--- app.py
+++ app.py
@@ -1,37 +1,29 @@
 import os
-import logging
+from logs.logging_config import LOG_CONFIG,setup_logging
 import argparse
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
-from flask_restx import Api, Resource
-from flask_caching import Cache
-from src.clock_calculator import calculate_angle_between_arrows
-from src.tools import validate_parameters
+from flask import jsonify
+from flask_restx import Resource
+from api_procedures import process_parameters, setup_flask, setup_api
+from src.utils import validate_parameters
+
 load_dotenv()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode",default="local")
 args = parser.parse_args()
 
-logging.basicConfig(filename='logs/app.log', level=logging.INFO,format='%(asctime)s -  %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-
-app = Flask(__name__)
-app.config["CACHE_TYPE"] = "SimpleCache"
-api = Api(app,title="Clock API",version="1.0",prefix="/vn/rest",
-          description="API aiming to calculate the smallest angle between clock's arrows.",)
-cache = Cache(app)
+app, cache = setup_flask()
+api = setup_api(app)
 
 @api.route("/calculate-clock/<hours>", defaults={"minutes": '0'})
 @api.route("/calculate-clock/<hours>/<minutes>")
-# @cache.cached(timeout=600)
+
 class DegreeCalculator(Resource):
     """
     Resource class to handle clock angle calculation.
     """
-
+    @cache.cached(timeout=600)
     def get(self, hours: str, minutes: str):
         """
         Calculate the smallest angle between the clock's arrows.
@@ -45,26 +37,13 @@ class DegreeCalculator(Resource):
         """
         
         params = validate_parameters(hours, minutes)
-        if isinstance(params.get("hours"), int) and isinstance(params.get("minutes"), int):
-            try:
-                params["angle"] = calculate_angle_between_arrows(params["hours"], params["minutes"])
-                
-                if args.mode == "local":
-                    from db.connector import dB_Cursor
-                    cursor = dB_Cursor()
-                    cursor.persist_result(params, request.remote_addr)
-                
-                result = {"angle": params["angle"]}
-            except Exception as e:
-                logger.warning(f"There was found an error: {e}")
-                result = {"Error found": str(e)}
-        else:
-            result = {"ERROR": params.get('message')}
+        result = process_parameters(params,args)
 
         return jsonify(result)
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("API_PORT"))
-    host = os.environ.get("API_HOST")
-    app.run(port=port,host = host, debug=False)
+    setup_logging(LOG_CONFIG)
+    app.run(port=os.environ.get("API_PORT"),
+            host = os.environ.get("API_HOST"),
+            debug=False)
